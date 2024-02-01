2ac16cc402d0c77b252827ab4b589ec955df5fca
QuoAPIs8 <72159813+QuoAPIs8@users.noreply.github.com>
2024-02-01 16:12:24 -0500

    Update script-2d-2024.js

--- script-2d-2024.js
+++ script-2d-2024.js
@@ -1423,9 +1423,13 @@ function init(){
                             result.features.forEach(el => {
                                 if(el && el.properties && el.properties.formatted){
                                     var $item = $(formatTextAutocomplete)
+                                    let addressget = [] 
+                                    if(el.properties.name)
+                                        addressget.push(el.properties.name)
+                                    if(el.properties.housenumber || el.properties.street)
+                                        addressget.push(el.properties.housenumber ?? "" + " " + el.properties.street ?? "")
                                     
-                                    
-                                    addressget = el.properties.address_line1
+                                    addressget = addressget.filter(Boolean).join(", ")
                                     this.currentAddress.push(addressget)
                                     $item.text(el.properties.formatted)
                                     $item.attr("data-address", addressget)
